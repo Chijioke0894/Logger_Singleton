aa1e61a773322d774bc9f7d45de321675dd1a7b5
Chijioke Ejiogu <ugocj2013@gmail.com>
2023-09-25 11:27:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Logger
+{
+    private Logger()
+    {
+    }
+    // private constructor to prevent direct instantiation
+
+    private static Logger instance;
+    //A single instance of the Logger is created
+
+    public static Logger GetInstance()
+    //static method that controls access to the singleton instance
+    {
+        if (instance == null)
+        {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void LogMessage(string text)
+    {
+        Console.WriteLine(text);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Logger logger = Logger.GetInstance();
+        logger.LogMessage("This is a test message");
+    }
+}
